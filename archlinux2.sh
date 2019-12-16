3af14b699f3e6e02755ac6e70671fc0f41c382b4
zahar77735 <zechara77@yandex.ru>
2019-12-16 20:15:01 +0300

    Update archlinux2.sh

--- archlinux2.sh
+++ archlinux2.sh
@@ -87,5 +87,5 @@ systemctl enable NetworkManager
 
 echo 'Установка завершена! Перезагрузите систему.'
 echo 'Если хотите подключить AUR, установить мои конфиги XFCE, тогда после перезагрзки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
-echo 'wget git.io/arch3.sh && sh arch3.sh'
+echo 'wget git.io/Je7Lz && mv Je7Lz lin3.sh'
 exit
